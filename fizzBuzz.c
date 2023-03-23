faf5a160c140926d6ff6ee9517194d845aa5ee69
Leonel Ricardo Carrizo Marin <lcarrizo@c3r5s1.42london.com>
2023-03-23 18:38:45 +0000

    more

---
+++ fizzBuzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void	fizzbuzz1(const int n)
+{
+	int	i = 0;
+
+	while (i < n)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 ==0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		printf("\n"); 
+		i++;
+	}
+}
+
+
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		printf("Usage: %s <# of rounds>\n", av[0]);
+		return (EXIT_FAILURE);
+	}
+	int	n = atoi(av[1]);
+
+	fizzbuzz1(n);
+	return (0);
+}
