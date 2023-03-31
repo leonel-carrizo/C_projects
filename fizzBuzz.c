46565fc8de22e88e7cbcb886572e415cc13dc7fc
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2023-03-31 20:19:31 +0100

    applying norminette test

--- fizzBuzz.c
+++ fizzBuzz.c
@@ -4,34 +4,34 @@
 
 void	fizzbuzz1(const int n)
 {
-	int	i = 0;
+	int	i;
 
+	i = 0;
 	while (i < n)
 	{
 		if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 ==0)
+		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else
 			printf("%d", i);
-		printf("\n"); 
+		printf("\n");
 		i++;
 	}
 }
 
-
-
 int	main(int ac, char **av)
 {
+	int	n;
+
 	if (ac != 2)
 	{
 		printf("Usage: %s <# of rounds>\n", av[0]);
 		return (EXIT_FAILURE);
 	}
-	int	n = atoi(av[1]);
-
+	n = atoi(av[1]);
 	fizzbuzz1(n);
 	return (0);
 }
